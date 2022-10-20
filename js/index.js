278b8414621638e23a144800750bfd977bdfdfda
giovanaveloso <giovanavelosog@gmail.com>
2022-10-20 09:16:52 -0300

    done

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ console.log("I'm ready!");
 // 1.1 Create a variable `hacker1` with the driver's name.
 let hacker1 = "Gabriela";
 // Print `"The driver's name is XXXX"`.
-console.log(`The driver's name is ${hacker1}`;
+console.log(`The driver's name is ${hacker1});
 
 // Create a variable `hacker2` with the navigator's name.
 let hacker2 = "Jon";
@@ -35,7 +35,7 @@ console.log(`The navigator's name is ${hacker2}`);
 */
 if (hacker1.length > hacker2) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
-    }
+    } 
 else if (hacker1.length > hacker2) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 }
